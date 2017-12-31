3e516e4fe7ff2f3aa3b433cf21a5eae3fa6d5514
Zack Toth <zacharyrtoth@gmail.com>
2017-12-30 22:05:04 -0800

    added test and challenge files, initial

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -27,6 +27,9 @@ BlogPosts.create(
 // and return JSON objects of stored blog posts.
 // send back JSON representation of all blog posts
 // on GET requests to root
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
 
 
 // add endpoint for POST requests, which should cause a new
@@ -35,6 +38,21 @@ BlogPosts.create(
 // the id, which `BlogPosts` will create. This endpoint should
 // send a 400 error if the post doesn't contain
 // `title`, `content`, and `author`
+router.post('/', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+  	req.body.title, req.body.content, req.body.author);
+  res.status(201).json(item);
+});
 
 
 // add endpoint for PUT requests to update blogposts. it should
@@ -43,7 +61,43 @@ BlogPosts.create(
 // the post matches the id of the path variable, and that the
 // following required fields are in request body: `id`, `title`,
 // `content`, `author`, `publishDate`
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = [
+  	'id', 'title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
+});
+
 
 // add endpoint for DELETE requests. These requests should
 // have an id as a URL path variable and call
 // `BlogPosts.delete()`
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post with id \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,42 @@ app.use(morgan('common'));
 
 // you need to import `blogPostsRouter` router and route
 // requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
+app.use('/blog-posts', blogPostsRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+//declare server
+let server;
+
+//create a function runServer that returns the promise 
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+//creates server.close as a promise 
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+//from challenge instructions for export to catch errors
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};

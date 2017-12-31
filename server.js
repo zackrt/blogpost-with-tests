4478f4b177def3820f97d88e7e0342aed054d922
Zack Toth <zacharyrtoth@gmail.com>
2017-12-30 22:22:28 -0800

    fixed errors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const blogPostsRouter = require('/blogPostsRouter');
+const blogPostsRouter = require('./blogPostsRouter');
 
 app.use(morgan('common'));
 
